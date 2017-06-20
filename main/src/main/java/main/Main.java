8302547f270e124a28838f45d1f0d27b02ebd0dd
maxim <maxim@MSI>
2017-06-20 10:04:41 +0200

    Audio available

--- main/src/main/java/main/Main.java
+++ main/src/main/java/main/Main.java
@@ -1,19 +1,23 @@
 package main;
 
-import model.dao.DAOTest;
-import view.MapMaker;
-import view.TranslateMap;
-import view.Window;
+import java.io.File;
+
+import model.dao.*;
+import view.Audio;
+import view.*;
 
 public class Main implements Runnable{
 	
 	static MapMaker maker = null;
+	static File music = null;
 	
 	public void run(){
 		System.out.println("Thread is running");
 		
 		maker.spritesCreation();
 		Window window = new Window(maker);
+		
+		Audio.PlaySound(music);
 	}
 		
 	public static void main(String[] args) {
@@ -23,12 +27,14 @@ public class Main implements Runnable{
 		connectionBDD.connection();
 		connectionBDD.executeQuery();
 		connectionBDD.setQueryIntoTable();
+		
 		TranslateMap translate = new TranslateMap(connectionBDD);
 		maker = new MapMaker(translate);
+		music = new File("music/BoulderDash.wav");
 		
 		Main m1 = new Main();
 		Thread t1 =new Thread(m1);  
-		t1.start(); 
+		t1.start();
 		
 	}
 }
